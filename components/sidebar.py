0ca70ef5ebd6e32162a88fcc301e6a1d736dd710
Ammaar Ahmad Khan <ammaarahmadkhan16@gmail.com>
2026-01-27 22:07:57 +0530

    Updated GitHub repo URL

--- components/sidebar.py
+++ components/sidebar.py
@@ -45,7 +45,7 @@ def sidebar_ui():
     st.sidebar.markdown(
         """
         [![Watch Demo](https://img.shields.io/badge/Watch-Demo%20Video-red?logo=youtube)](https://youtu.be/Yd0xMocB-V0)
-        [![GitHub](https://img.shields.io/badge/GitHub-Repo-181717?logo=github)](https://github.com/GPA95/AI_StudyBuddy)
+        [![GitHub](https://img.shields.io/badge/GitHub-Repo-181717?logo=github)](https://github.com/GPA95/SGPA)
         [![HelpDoc](https://img.shields.io/badge/User%20Help-How%20to%20Use-blue?logo=google-drive)](https://drive.google.com/file/d/14NEdW6L4WC_hiqlhNBEElzJnfN60EZDa/view?usp=sharing)
         """
     )
